5b47859c773513c2923888aeeb93f46c6eaa264b
rseonp <Victor@victors-mbp.resnet.wustl.edu>
2016-02-29 20:42:53 -0600

    Initial Commit

---
+++ Instagram2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram2.0
+//
+//  Created by Victor Li Wang on 2/29/16.
+//  Copyright © 2016 Victor Li Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
